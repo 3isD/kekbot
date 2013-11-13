913ac138be4d76615242d70e92c716c2c6708f1e
Ivan Kondratyev <strategetical@gmail.com>
2013-11-13 12:51:01 -0600

    testing_imports
    Just some quick tests.

--- import.js
+++ import.js
@@ -5,7 +5,7 @@ kbdiv.setAttribute("id","KekBot");
 kbdiv.setAttribute("style","display:none;");
 var kbver = document.createElement("script");
 kbver.setAttribute("class","kbversion");
-kbver.setAttribute("src","http://files.sq10.net/UPLOADED_FILES/1384362018034.js");
+kbver.setAttribute("src","https://raw.github.com/Strategetical/kekbot/master/info.js");
 kbdiv.appendChild(kbver);
 var bodelm = document.getElementsByTagName("body")[0];
 bodelm.insertBefore(kbdiv,bodelm.firstChild);
--- info.js
+++ info.js
@@ -1,11 +1,20 @@
 (function(){
 	window.kbinf = {};
-	kbinf.buildnum = 20;
+	kbinf.buildnum = 10;
 	try{
-		kekbot.forceUpdate();
+		if (kbinf.buildnum > kekbot.buildnum){
+			alert("Gotta update!");
+		}
 	}catch(e){
 		alert("KekBot: Could not update. See error in console.");
 		console.log("KekbotERROR:");
-		console.log(e);
+		console.log(e);b n
 	}
+	alert("Done.");
+	var kbv = document.getElementById("KekBot").getElementsByClassName("kbversion")[0];
+	kbv.parentNode.removeChild(kbv);
+	var kbver = document.createElement("script");
+	kbver.setAttribute("class","kbversion");
+	kbver.setAttribute("src","https://raw.github.com/Strategetical/kekbot/master/info.js");
+	document.getElementById("KekBot").appendChild(kbver);
 })()
