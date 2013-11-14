6512c031a34cd03256053874fab5d527acddc430
Ivan Kondratyev <strategetical@gmail.com>
2013-11-14 13:59:00 -0600

    quick_change
    made updating more verbose

--- info.js
+++ info.js
@@ -1,5 +1,5 @@
 kbinf = {};
-kbinf.buildnum = 11;
+kbinf.buildnum = 12;
 try{
 	if (kbinf.buildnum > kekbot.buildnum){
 		kekbot.say("Updating KekBot from build number "+kekbot.buildnum+" to "+kbinf.buildnum);
--- kekbot.js
+++ kekbot.js
@@ -32,7 +32,7 @@ var kekbot = {};
 //Define the basic settings of Kekbot.
 kekbot.alias = "[KB]";
 kekbot.version = "2.0beta2";
-kekbot.buildnum = 11;
+kekbot.buildnum = 12;
 
 //Kekbot user functions. NOT to be confused with kekbot.users, which stores the users.
 kekbot.user = {};
@@ -78,6 +78,7 @@ kekbot.init = function(){
 }
 
 kekbot.update = function(){
+	kekbot.say("Checking for updates.");
 	var kbver = document.createElement("script");
 	kbver.setAttribute("class","kbversion");
 	kbver.setAttribute("src","https://raw.github.com/Strategetical/kekbot/master/info.js");
