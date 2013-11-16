9333397af5dbb039a7867acfae2ae9afb9655dac
Ivan Kondratyev <strategetical@gmail.com>
2013-11-15 18:01:40 -0600

    Create info.js

---
+++ plugins/core/info.js
@@ -0,0 +1,19 @@
+/*
+Kekbot core plugin info.
+
+Right now it's for testing the v2 plugin system.
+*/
+
+(function(){
+  var build = 1;
+  var name = "core";
+  var url = "https://raw.github.com/Strategetical/kekbot/master/plugins/core/plugin.js";
+  
+  try{
+    if(kb_plugins.installedPlugins[name].version >= build){
+      return false;
+    }
+  }
+  catch(e){}
+  kekbot.plugin.install(name, url);
+})();
