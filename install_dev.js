8e1713402831739f536befd3062ac61acafabeb3
Ivan Kondratyev <strategetical@gmail.com>
2013-11-16 18:34:02 -0600

    install_dev
    For my testing purposes.

---
+++ install_dev.js
@@ -0,0 +1,40 @@
+"use strict";
+function prepareKekbot(){
+	var kbdiv = document.createElement("div");
+	kbdiv.setAttribute("id","KekBot");
+	kbdiv.setAttribute("style","display:none;");
+	var kbplugins = document.createElement("div");
+	kbplugins.setAttribute("id","KB_Plugins");
+	var kbpluginsinfo = document.createElement("div");
+	kbpluginsinfo.setAttribute("id","KB_PluginsInfo");
+	kbplugins.appendChild(kbpluginsinfo);
+	var kbver = document.createElement("script");
+	kbver.setAttribute("id","KB_Version");
+	kbver.setAttribute("src","https://raw.github.com/Strategetical/kekbot/master/dev/info.js");
+	kbdiv.appendChild(kbver);
+	kbdiv.appendChild(kbplugins);
+	var bodelm = document.getElementsByTagName("body")[0];
+	bodelm.insertBefore(kbdiv,bodelm.firstChild);
+	window.installKekbot = function(){
+		window.kb_plugins = {};
+		kb_plugins.installedPlugins = {};
+		kb_plugins.pendingPlugins = {};
+		kb_plugins.activePlugins = {};
+		var newkb = document.createElement("script");
+		newkb.setAttribute("class", "KB_"+kbinf.buildnum);
+		newkb.setAttribute("src", "https://raw.github.com/Strategetical/kekbot/master/dev/kekbot.js");
+		document.getElementById("KekBot").appendChild(newkb);
+		installKekbot = undefined;
+	}
+	prepareKekbot = undefined;
+}
+try{
+	kekbot.update();
+}
+catch(e){
+	prepareKekbot();
+}
+try{
+	var kbid = document.getElementById("KB_Import");
+	kbid.parentNode.removeChild(kbid);
+}catch(e){}
