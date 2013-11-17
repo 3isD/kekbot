035a21e478489e88feba3b2809528b0a17d3f960
Ivan Kondratyev <strategetical@gmail.com>
2013-11-16 18:32:17 -0600

    plugin_support
    todo: have plugin uninstallers, etc.

--- dev/kekbot.js
+++ dev/kekbot.js
@@ -53,10 +53,35 @@ kekbot.test.mod = function(userid, admin){
 	else{return false;}
 }
 
+//Kekbot listeners (added by plugins)
+kekbot.listeners = {};
+kekbot.listeners["command"] = {};
+kekbot.listeners["message"] = [];
+
 //Kekbot handlers.
 kekbot.handle = {};
 kekbot.handle.chat = function(data){
 	//Track the user.
+	switch(data.type){
+		case "message":
+			var str = data.message;
+			var command = str.substr(0, str.indexOf(' '));
+			if(kekbot.listeners["command"][command]){
+				for (func in kekbot.listeners["command"][command]){
+					try{
+						kekbot.listeners["command"][str1][func](data);
+					}catch(e){}
+				}
+			}
+			for (func in kekbot.listeners["message"]){
+				try{
+					kekbot.listeners["message"][func](data);
+				}catch(e){}
+			}
+			break;
+		default:
+			break;
+	}
 }
 
 //Kekbot user handlers.
@@ -77,7 +102,7 @@ kekbot.handle.user.Skip = function(data){
 
 //Kekbot core functions.
 kekbot.implode = function(){
-	console.log("Kekbot: Imploding. Bye bye!");
+	API.chatLog("Kekbot: Imploding. Bye bye!", true);
 	API.off(API.CHAT, kekbot.handle.chat);
 	API.off(API.USER_JOIN, kekbot.handle.user.Join);
 	API.off(API.USER_LEAVE, kekbot.handle.user.Leave);
@@ -90,17 +115,18 @@ kekbot.implode = function(){
 }
 
 kekbot.init = function(){
-	console.log("Kekbot: Init.");
+	API.chatLog("Kekbot: Init.", true);
 	API.on(API.CHAT, kekbot.handle.chat);
 	API.on(API.USER_JOIN, kekbot.handle.user.Join);
 	API.on(API.USER_LEAVE, kekbot.handle.user.Jeave);
 	API.on(API.USER_FAN, kekbot.handle.user.Fan);
 	API.on(API.USER_SKIP, kekbot.handle.user.Skip);
 	kekbot.updateTimer = setInterval(kekbot.update, 1000*60*10);
+	//update all plugins.
 }
 
 kekbot.update = function(){
-	console.log("Checking for updates.");
+	API.chatLog("Checking for updates.", true);
 	var kbver = document.createElement("script");
 	kbver.setAttribute("id","KB_Version");
 	kbver.setAttribute("src","https://raw.github.com/Strategetical/kekbot/master/core/info.js");
@@ -138,44 +164,120 @@ kekbot.testURL = function(str){
 kekbot.plugin = {};
 kekbot.plugin.add = function(obj){
 	if(typeof obj != "object"){
-		console.log("KekBot_Plugins: [add] Passed variable is not an object. Quitting.");
+		API.chatLog("KekBot_Plugins: [add] Passed variable is not an object. Quitting.", true);
 		return false;
 	}
 	if(!obj.name || !obj.url){
-		console.log("KekBot_Plugins: [add] Object doesn't have either a name or url attribute.");
+		API.chatLog("KekBot_Plugins: [add] Object doesn't have either a name or url attribute.", true);
 		return false;
 	}
 	if(!((typeof obj.name == "string") && (typeof obj.url == "string"))){
-		console.log("KekBot_Plugins: [add] Both the plugin name and URL MUST be string!");
+		API.chatLog("KekBot_Plugins: [add] Both the plugin name and URL MUST be string!", true);
 		return false;
 	}
 	if (!(/^[a-z]+$/i.test(obj.name))){
-		console.log("KekBot_Plugins: [add] Plugin name MUST be alphabetic characters only.");
+		API.chatLog("KekBot_Plugins: [add] Plugin name MUST be alphabetic characters only.", true);
 		return false;
     	}
     	if (!kekbot.testURL(obj.url)){
-    		console.log("KekBot_Plugins: [add] Plugin URL MUST be a valid URL!");
+    		API.chatLog("KekBot_Plugins: [add] Plugin URL MUST be a valid URL!", true);
     		return false;
     	}
     	for (item in kb_plugins.installedPlugins){
     		if (obj.name == item){
-    			console.log("KekBot_Plugins: [add] A plugin with the name "+obj.name+" is already installed!");
+    			API.chatLog("KekBot_Plugins: [add] A plugin with the name "+obj.name+" is already installed!", true);
     			return false;
     		}
     		if (obj.url == kb_plugins[item]){
-			console.log("KekBot_Plugins: [add] A plugin's update URL is the same with another plugin's.");
+			API.chatLog("KekBot_Plugins: [add] A plugin's update URL is the same with another plugin's.", true);
 			return false;
     		}
     	}
-    	kekbot.installedPlugins[obj.name] = obj.url;
+    	kb_plugins.installedPlugins[obj.name] = {
+    		url: obj.url
+    	};
     	kekbot.plugin.update(obj.name);
 }
 kekbot.plugin.update = function(p){
-	var plugin = document.createElement("script");
-	plugin.setAttribute("id", "KB_PluginInfo_"+p);
-	plugin.setAttribute("src", kb_plugins.installedPlugins[p]);
-	document.getElementById("KB_PluginsInfo").appendChild(plugin);
+	try{
+		kb_plugins.installedPlugins[p].installed = false;
+	}catch(e){
+		API.chatLog("KekBot_Plugins: [update] Cannot update a plugin that isn't installed!");
+		return false;
+	}
+	var plugininfo = document.createElement("script");
+	plugininfo.setAttribute("id", "KB_PluginInfo_"+p);
+	plugininfo.setAttribute("src", kb_plugins.installedPlugins[p].url);
+	document.getElementById("KB_PluginsInfo").appendChild(plugininfo);
 }
-
+kekbot.plugin.install = function(name, url){
+	try{
+		var x = document.getElementById("id", "KB_PluginInfo_"+name);
+		x.parentNode.removeChild(x);
+	}
+	catch(e){
+		API.chatLog("KekBot: could not remove plugin version info during installation (not supposed to happen)!", true);
+	}
+	var plugin = document.createElement("script");
+	plugin.setAttribute("id", "KB_Plugin_"+name);
+	plugin.setAttribute("src", url);
+	document.getElementById("KB_Plugins").appendChild(plugin);
+}
+kekbot.plugin.verifyInstall = function(name){
+	try{
+		kb_plugins.activePlugins[name] = kb_plugins.pendingPlugins[name];
+		kb_plugins.installedPlugins[name].installed = true;
+		delete kb_plugins.pendingPlugins[name];
+	}
+	catch(e){
+		API.chatLog("Could not install the plugin "+name+"!", true);
+		API.chatLog("Error: "+e, true);
+		try{
+			delete kb_plugins.pendingPlugins[name];
+			delete kb_plugins.activePlugins[name];
+		}catch(e){}
+	}
+}
+kekbot.plugin.addListener = function(obj){
+	try{
+		switch(obj.type){
+			case "command":
+				if(!kekbot.listeners[obj.type][obj.command]){
+					kekbot.listeners[obj.type][obj.command] = [];
+				}
+				kekbot.listeners[obj.type][obj.command].push(kb_plugins.activePlugins[obj.plugin][obj.name]);
+				return {type: obj.type, command: obj.command, index: kekbot.listeners[obj.type].length-1};
+			case "message":
+				kekbot.listeners[obj.type].push(kb_plugins.activePlugins[obj.plugin][obj.name]);
+				return {type: obj.type, index: kekbot.listeners[obj.type].length-1};
+			default:
+				kekbot.chatLog("Could not add plugin listener: unsupported type.");
+				return false;
+		}
+	}
+	catch(e){
+		API.chatLog("Could not add plugin listener.", true);
+		API.chatLog("Type: "+type+" Plugin: "+plugin+" Name: "+name+" Error: "+e, true);
+		return false;
+	}
+}
+kekbot.plugin.removeListener = function(obj){
+	try{
+		switch(obj.type){
+			case "command":
+				kekbot.listeners[obj.type][obj.command][obj.index] = function(a){};
+			case "message":
+				kekbot.listeners[obj.type][obj.index] = function(a){};
+			default:
+				kekbot.chatLog("Could not remove plugin listener: unsupported type.");
+				return false;
+		}
+		
+	}
+	catch(e){
+		API.chatLog("Could not remove plugin listener.", true);
+		API.chatLog("Error: "+e, true);
+	}
+}
 kekbot.say("KekBot: Installed. v"+kekbot.version+" BuildNum #"+kekbot.buildnum);
 kekbot.init();
