89223e2867ae309b36db62c95af412b960a9948e
Ivan Kondratyev <strategetical@gmail.com>
2013-11-16 18:50:03 -0600

    testing_core
    Testing core plugin.

--- plugins/core/plugin.js
+++ plugins/core/plugin.js
@@ -1,5 +1,26 @@
 /*
 Kekbot core plugin.
-
-I'll have something here eventually.
 */
+
+(function(){
+  var name = "core";
+  var build = 1;
+  var version = "0.1dev";
+  
+  kb_installedPlugins[name].build = build;
+  kb_installedPlugins[name].version = version;
+  
+  kb_pendingPlugins[name] = {
+    roll: function(data){
+      kekbot.say("@"+data.username+" just did a barrel roll");
+    }
+  };
+  kekbot.plugin.addListener({
+    type: "command",
+    command: "^roll",
+    plugin: name,
+    name: "say"
+  });
+  
+  kekbot.plugin.verifyInstall(name);
+})()
